fa96a97eaec96475fe67de30e5559fe40588f60c
Carsten Nielsen <carsten@redpill-linpro.com>
2018-05-08 10:42:18 +0200

    Update gulp-tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,29 @@
 // Include gulp
 var gulp = require('gulp');
 
+var del = require('del');
+
 var inline = require('gulp-inline')
   , uglify = require('gulp-uglify')
   , minifyCss = require('gulp-minify-css')
   , autoprefixer = require('gulp-autoprefixer');
- 
-gulp.task('inline-it', function() {
-  return gulp.src('./index.html')
+
+gulp.task('clean', function () {
+  return del([
+    'dist/index.html'
+  ]);
+});
+
+gulp.task('compile', function() {
+  return gulp.src('index.html')
     .pipe(inline({
       base: '.',
-      js: uglify,
-      css: [minifyCss, autoprefixer({ browsers:['last 2 versions'] })],
+      //js: uglify,
+      //css: [minifyCss, autoprefixer({ browsers:['last 2 versions'] })],
       disabledTypes: ['svg', 'img'], // Only inline css files
-      ignore: ['./gulp.js']
+      ignore: ['gulp.js']
     }))
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', ['inline-it']); 
+gulp.task('default', gulp.series(['clean', 'compile'])); 
